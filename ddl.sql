a682843c780391ebb3e80bf1d8a78bda762ffa7d
tyrada <93895327+tyrada@users.noreply.github.com>
2023-11-24 02:04:09 -0500

    Delete ddl.sql

--- ddl.sql
+++
@@ -1,8 +0,0 @@
---create student table
-CREATE TABLE students (
-    student_id serial PRIMARY KEY,
-    first_name text NOT NULL,
-    last_name text NOT NULL,
-    email text NOT NULL UNIQUE,
-    enrollment_date date
-);
