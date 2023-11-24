aa8ec46e4efa028cc044254414c9538d3e01f93f
tyrada <93895327+tyrada@users.noreply.github.com>
2023-11-24 00:17:24 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
-import json
+#import json
 import psycopg2
 from tabulate import tabulate
 from contextlib import closing
-
+'''
 # import the database configuration from config.json
 with open('config.json', 'r') as config_file:
     parameters = json.load(config_file)
+'''
+conn = psycopg2.connect(database = "postgres", 
+                        user = "postgres", 
+                        host= 'localhost',
+                        password = "student",
+                        port = 5432)
 
+cur = conn.cursor()
+'''
+Retrieves and displays all tuples from the students table.
+params: none
+returns: a tabular display of all the students in the database
+'''
 def getAllStudents():
+    cur = conn.cursor() 
+    cur.execute("SELECT * FROM students") #sql way of choosing all data in the students table
+    rows = cur.fetchall() #fetch the data to be printed (all data)
+    conn.commit() #commit to the changes 
+    cur.close() #close teh cursor and shut down the connection
+    conn.close()
+    for row in rows:    #loop through to print the data
+        print(row)
+    '''
     try:
         with closing(psycopg2.connect(**parameters)) as connection:
             with closing(connection.cursor()) as cursor:
@@ -15,10 +36,17 @@ def getAllStudents():
                 cursor.execute(query_string)
                 students = cursor.fetchall()
                 return students
-    except Exception as e:
+    #in the case of an error, display the error and return an empty table of no students
+    except Exception as e: 
         print(f"An error occurred: {e}")
         return []
+        '''
 
+'''
+Adds a student into the student table
+params: first name, last name, email, enrollment date of the student to populate the tuple
+returns: a message to display if it was successfully added or not 
+'''
 def addStudent(first_name, last_name, email, enrollment_date):
     try:
         with closing(psycopg2.connect(**parameters)) as connection:
@@ -35,6 +63,11 @@ def addStudent(first_name, last_name, email, enrollment_date):
     except Exception as e:
         print(f"An error occurred: {e}")
 
+'''
+Updates an existing students' email
+params: the id of th student to update and the new email to update the old email from
+returns: a message to display if it was successfully updated or not 
+'''
 def updateStudentEmail(student_id, new_email):
     try:
         with closing(psycopg2.connect(**parameters)) as connection:
@@ -48,10 +81,15 @@ def updateStudentEmail(student_id, new_email):
                 connection.commit()
 
                 print("Student email updated successfully")
-
+    #would be executed if the student does not exist or email to be updated is not unique (email already exists to another student)
     except Exception as e:
         print(f"An error occurred: {e}")
 
+'''
+deletes an existing student's data
+params: the id of th student to delete
+returns: a message to display if it was successfully deleted or not 
+'''
 def deleteStudent(student_id):
     try:
         with closing(psycopg2.connect(**parameters)) as connection:
@@ -69,11 +107,12 @@ def deleteStudent(student_id):
         print(f"An error occurred: {e}")
 
 def main():
+    #unprompted testing
     # TESTING: getAllStudents()
     print(f'\nTESTING: getAllStudents():')
-    headers = ["ID", "First Name", "Last Name", "Email", "Date"]
-    print(tabulate(getAllStudents(), headers=headers, tablefmt="grid"))
-
+    headers = ["ID", "First Name", "Last Name", "Email", "Date"] #param to display columns
+    print(tabulate(getAllStudents(), headers=headers, tablefmt="grid"))#displays updated table
+    '''
     # TESTING: addStudent()
     print(f'\nTESTING: addStudent():')
     addStudent('Muntaha', 'Attef', 'muntahaattef@cmail.carleton.ca', '2023-11-23')
@@ -87,6 +126,9 @@ def main():
     # TESTING: deleteStudent()
     print(f'\nTESTING: deleteStudent():')
     deleteStudent('4')
-    print(tabulate(getAllStudents(), headers=headers, tablefmt="grid"))
+    print(tabulate(getAllStudents(), headers=headers, tablefmt="grid")) 
+
+    #prompted testing
+    '''
 
 main()
